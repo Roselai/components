a7564ac52b02458eee274335ad1815474a6fdec5
Shukti Shaikh <shukti.azad@gmail.com>
2019-04-12 15:43:48 -0400

    added avatar props

--- src/index.js
+++ src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from './CommentDetail'
 const App = () => {
 	return (
 		<div className="ui container comments">
-			<CommentDetail author="Sam" timeAgo="Today at 4:45PM" message="props are cool"/>
-			<CommentDetail author="Alex" timeAgo="Today at 2:00AM" message="you're cool"/>
-			<CommentDetail author="Jane" timeAgo="Today at 5:00PM" message="But I'm the coolest"/>
+			<CommentDetail 
+				author="Sam" 
+				timeAgo="Today at 4:45PM" 
+				message="props are cool" 
+				avatar={faker.image.avatar()}
+			/>
+			<CommentDetail 
+				author="Alex" 
+				timeAgo="Today at 2:00AM" 
+				message="you're cool" 
+				avatar={faker.image.avatar()}
+			/>
+			<CommentDetail 
+				author="Jane" 
+				timeAgo="Today at 5:00PM" 
+				message="But I'm the coolest" 
+				avatar={faker.image.avatar()}
+			/>
 		</div>
 	);
 };
